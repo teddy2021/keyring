a5a50c85f0c60bfcf895f7189cac89b385a56e34
teddy2021 <darkking@live.ca>
2019-06-25 21:41:55 -0600

    fleshed out pageinfo.js, need to build main system and webpage interaction

--- extension/pageinfo.js
+++ extension/pageinfo.js
@@ -1,14 +1,18 @@
 
-var url = require URL;
+
+var url = require(url);
 
 var page = url.hostname(document.Location);
 
-$onReady () => {
+$document.onReady() =>{
 	$.ajax({
-		data: page
-		url: "localhost:8080"
+		data: page,
+		url: "localhost:8080",
 		success: () => {
-
+			$("#Username").val(this.username);
+			$("#Password").val(this.password);
 		}
-	})
-}
+	});
+};
+
+
